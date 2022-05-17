2aa6eec5fc27d99bf25333a456f9e4d25cb72426
Daniel Kelly <me@danielkelly.io>
2022-05-17 14:13:57 -0500

    5-volar-the-official-language-feature-extension-for-vs-code

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,21 @@
 <script setup lang="ts">
+import {ref} from "vue";
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from '@/components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue';
+import IconCommunityChanged from './components/icons/IconCommunity.vue';
+const test = ref("test");
+test.value = 'something else'
 </script>
 
 <template>
   <header>
+    {{test}}
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
+    <TheWelcome/>
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
-
+      <IconCommunityChanged/>
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
