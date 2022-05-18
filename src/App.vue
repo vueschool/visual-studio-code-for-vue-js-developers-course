16cc1fabfd4b919dc7761d704777b0fb9b991dc4
Daniel Kelly <me@danielkelly.io>
2022-05-18 10:19:27 -0500

    7-auto-formatting-files-in-vs-code-volar-built-in-or-prettier

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,23 @@
 <script setup lang="ts">
-import {ref} from "vue";
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from '@/components/HelloWorld.vue'
+import { ref } from 'vue';
+import { RouterLink, RouterView } from 'vue-router';
+import HelloWorld from '@/components/HelloWorld.vue';
 
-const test = ref("test");
-test.value = 'something else'
+const test = ref('test');
+test.value = 'something else';
 </script>
 
 <template>
   <header>
-    {{test}}
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-    
+    {{ test }}
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="@/assets/logo.svg"
+      width="125"
+      height="125"
+    />
+
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
       <nav>
