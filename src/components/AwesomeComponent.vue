24214b676b9704c1c8f612752d9ffc03edece500
Daniel Kelly <me@danielkelly.io>
2022-06-15 14:55:25 -0500

    15-styling-vue-components-in-vs-code-css

--- src/components/AwesomeComponent.vue
+++ src/components/AwesomeComponent.vue
@@ -1,5 +1,10 @@
 <template>
-  <div>
+  <div class="bg-lime-900 p-12">
     <span class="fa-twitter">Hello World</span>
   </div>
 </template>
+<style>
+div {
+  padding: 20% !important;
+}
+</style>
