03a435d9920e2ffb3d5220e7d79ba2ae1fcca8fd
Daniel Kelly <me@danielkelly.io>
2022-06-29 12:27:44 -0500

    20-extensions-honorable-mentions

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,9 @@ import router from './router';
 
 const app = createApp(App);
 
+// TODO: something is left undone, fix it!
 app.use(createPinia());
 app.use(router);
 
 app.mount('#app');
+// TODO: Something else
